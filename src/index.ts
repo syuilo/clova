f2175ff8e4c684bd67fa5966b005a9afe8888e14
syuilo <Syuilotan@yahoo.co.jp>
2019-10-06 16:15:52 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -98,6 +98,12 @@ export class Context {
 	public async showCardChoices(target: number, cards: Card[]) {
 		// TODO
 		const choice = Math.floor(Math.random() * cards.length);
+
+		this.commit({ type: 'choiceCard', payload: {
+			player: this.currentPlayer,
+			cardId: cards[choice].id,
+		}});
+
 		return cards[choice];
 	}
 }
