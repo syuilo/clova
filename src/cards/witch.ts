fc02e56398f032818176454156034d23c59b7edf
syuilo <Syuilotan@yahoo.co.jp>
2019-10-16 20:52:33 +0900

    Update witch.ts

--- src/cards/witch.ts
+++ src/cards/witch.ts
@@ -3,11 +3,13 @@ import { CardDef } from '../engine';
 export default {
 	id: '2902ba9e-f1ca-4e25-8320-674fc75b7bd4',
 	name: '魔法使い',
+	desc: 'このユニットがフィールドにある限り、自分のターン終了ごとに「火の魔法」「癒しの魔法」「強化の魔法」のいずれかを選び手札に加える。',
 	image: 'https://2.bp.blogspot.com/-9E31gBPeGKI/U00KHPHLXqI/AAAAAAAAfOg/VfOxsQp8w-8/s800/majo_girl_majokko.png',
 	type: 'unit' as const,
 	power: 2,
 	cost: 5,
 	attrs: [],
 	setup: async (game, thisCard) => {
+		// TODO
 	}
 } as CardDef;
